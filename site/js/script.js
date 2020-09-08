368944d4215c53fb6e4024a4a60c286ecc1c4b79
Ivan Pesenti <ivan.pesenti@abenergie.it>
2020-09-08 08:52:51 +0200

    Make the home page populated via AJAX requests
    Now the home page is no more static but it has been populated
    dynamically.

--- site/js/script.js
+++ site/js/script.js
@@ -8,3 +8,41 @@ $(function(){ // Same as document.addEventListener("DOMContentLoaded"...)
         }
     });
 });
+
+
+(function (global) {
+    
+    var dc = {};
+
+    var homeHtml = "snippets/home-snippet.html";
+
+    // Convenience method for inserting innerHTML for 'select'
+    var insertHtml = function (selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHtml = html;
+    };
+
+    // Show loading icon inside element identified by 'selector'.
+    var showLoading = function(selector) {
+        var html = "<div class='text-center'>";
+        html += "<img src='images/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    };
+
+    // On page load (before images or CSS)
+    document.addEventListener("DOMContentLoaded", function (event) {
+
+        // On first load, show home view
+        showLoading("#main-content");
+    
+        $ajaxUtils.sendGetRequest(
+            homeHtml,
+            function (responseText) {
+                document.querySelector("#main-content").innerHTML = responseText;
+            },
+            false);
+    });    
+
+    global.$dc = dc;
+
+})(window);
